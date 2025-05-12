c5901194de64e53d6ac81d44c2a2c466838bd292
Guilherme Pereira da Silva Filho <guilherminhupereira07@gmail.com>
2025-05-12 08:03:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+def analisar_numeros(lista):
+    if not lista:
+        return "A lista não pode estar vazia"
+    
+    media = sum(lista) / len(lista)        
+    maior = max(lista)                     
+    menor = min(lista)                    
+    pares = len([n for n in lista if n % 2 == 0])  
 
+    return media, maior, menor, pares
